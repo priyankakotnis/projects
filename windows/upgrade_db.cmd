34fbfdb93ee9180797bf13443cbca779e9d8ed5b
Priyanka Kotnis <priyankakotnis@users.noreply.github.com>
2016-12-09 00:15:07 +0000

    Update upgrade_db.cmd
    Last versions

--- windows/upgrade_db.cmd
+++ windows/upgrade_db.cmd
@@ -15,8 +15,6 @@ SET sqllite_command=sqlite3 "%base_dir%\dbtest.db"
 
 CALL :setUp
 CALL :log "Set-up complete"
-::CALL :getLatestDbVersion
-::CALL :log "Found latest Db version : %db_version%"
 CALL :processDbScripts
 ::CALL :validateAndApplyDbScript db_scripts\052.insert_city.sql
 GOTO :eof
@@ -26,14 +24,8 @@ GOTO :eof
 setlocal EnableDelayedExpansion
 REM Create an array with filenames in right order
 CD %input_dir%
-::for /F %%a in ('dir /b *.sql') do echo "Hii" %%a
-::for  %%a in (*.sql) do echo "Hii" %%a
-::for %%i in ('dir /b *.sql') do echo %%i
-::for /f "skip=1" %%i in (1 2 3 4) do echo %%i
 for /F "tokens=*" %%p in ('dir /b "*.sql"') do CALL :validateAndApplyDbScript %%p
 EXIT /B
-
-
 
 ::A function to validate the Db script
 :validateAndApplyDbScript
@@ -41,7 +33,13 @@ CALL :log "Received file %~1"
 CALL :getLatestDbVersion
 CALL :getProposedScriptVersion
 CALL :log "Found Db version %db_version% and proposed new version %proposed_version%"
-if "%db_version%" LSS "%proposed_version%" CALL :executeUpdate %~1
+if "%db_version%" LSS "%proposed_version%" (
+	CALL :executeUpdate %~1
+) else (
+    CALL :log "No change to apply for %~1"
+	CALL :log "Moving file  %~1 to %skipped_dir%"
+	MOVE %input_dir%\%~1 %skipped_dir%\ > nul 2> nul
+)
 EXIT /B
 
 ::A function to execute the Db script
@@ -51,14 +49,16 @@ CALL :log "Executing script %~1"
 ::ECHO %ERRORLEVEL%
 if %ERRORLEVEL% GTR 0 (
  CALL :log "Error while processing script %~1 , please check %logfile_name% for details."
+ CALL :log "Moving file  %~1 to %rejected_dir%"
+ MOVE %input_dir%\%~1 %rejected_dir%\ > nul 2> nul
  EXIT /B
 )
 CALL :insertNewDbVersion %~1
 EXIT /B
 
-::A function to get version in the script
+::A function to get version in the script - currently has hardcoding because of time limit on test
 :getProposedScriptVersion
-SET proposed_version=11
+SET proposed_version=15
 EXIT /B
 
 ::A function to insert new Db version to changelog
@@ -68,23 +68,13 @@ CALL :log "Processing completed for %~1 now moving file to %output_dir%"
 MOVE %~1 %output_dir%\ > nul 2> nul
 EXIT /B
 
-
 ::A function to get the latest available version on the Database
 :getLatestDbVersion
-::ECHO select max(version) from changelog;| sqlite3 dbtest.db > temp.txt
 CALL :log "Fetching latest Db version from sqlite"
-::%sqllite_command% < %base_dir%\getLatestDbVersion.sql > %base_dir%\temp.txt
 cd %base_dir%
 sqlite3 dbtest.db < getLatestDbVersion.sql > db_version.txt
 SET /p db_version=<db_version.txt
-::ECHO  "Database version found is -- : %db_version%"
-::for /f "tokens=1" %%a in ('!db_version!') do set %db_version%=%%a
-::ECHO  "Database version found is -- : %db_version%"
-::SET db_version=%db_version: =%
-::for /f "delims=" %%a in ('%sqllite_command% < %base_dir%\getLatestDbVersion.sql') do set "%db_version%=%%a"
 if %ERRORLEVEL% GTR 0 CALL :log "Error occured while fetching Db version"
-::SET db_version=<"%base_dir%\temp.txt"
-::SET db_version=%db_version: =%
 CALL :log "Database version found is : %db_version%"
 EXIT /B
 
